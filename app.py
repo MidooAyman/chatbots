2f3e36ee141eaa7629446dbf9c8df287bfcab8b0
unknown <mohamedayman922002@gmail.com>
2023-12-11 03:36:56 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.json['message']
+    
+    # Simple response logic
+    if 'اسمك' in user_message:
+        reply = 'اسمي محمد.'
+    elif 'عمرك' in user_message:
+        reply = 'عمري 22 عاما.'
+    elif 'كليتك' in user_message or 'اين تدرس' in user_message or 'كلية ايه' in user_message:
+        reply = 'كلية التكنولوجيا و التنمية.'
+    else:
+        reply = 'لا أستطيع فهم السؤال. يمكنك أن تسألني عن اسمي أو عمري.'
+
+    return jsonify({'reply': reply})
+
+if __name__ == '__main__':
+    app.run(debug=True)
